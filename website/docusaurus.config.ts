506bc08b32370eae7bfab18b064e88933901d1ad
0xAly <280280231+0xAly@users.noreply.github.com>
2026-06-12 11:37:06 +0400

    fix(docs): polish docs header and footer

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -143,7 +143,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} KemetResearch. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} KemetResearch.`,
     },
     prism: {
       theme: prismThemes.github,
